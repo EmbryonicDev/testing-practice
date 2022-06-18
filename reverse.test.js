671cf2e1654718c14cb3e99f30f802f541b911e3
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-06-18 08:06:53 +0200

    Add test for reversing a multiple word string

--- reverse.test.js
+++ reverse.test.js
@@ -3,3 +3,7 @@ const reverse = require('./reverse')
 test('reverse single word', () => {
   expect(reverse("sdrawkcab")).toMatch(/backwards/)
 });
+
+test('reverse multiple word string', () => {
+  expect(reverse('desrever neeb sah gnirts siht')).toMatch(/this string has been reversed/)
+});
