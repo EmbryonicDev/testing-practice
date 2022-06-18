dc1622e6bfeb0b1e64ec4ec6108a08f67ec30947
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-06-18 12:33:14 +0200

    Add test: returns error if an argument is not a number

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,7 @@ const calculator = require('./calculator');
 test('adds two numbers', () => {
   expect(calculator.add(3, 9)).toBe(12);
 });
+
+test('returns error if an argument is not a number', () => {
+  expect(calculator.add('3', 6)).toMatch(/Please enter two numbers/)
+})
