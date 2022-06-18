bb7a41af1baf376048315b9eb64bd38eb15169e3
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-06-18 12:39:29 +0200

    Write tests for calculator.subtract
    Test: subtract one number from another
    Test: returns error if an argument is not a number

--- calculator.test.js
+++ calculator.test.js
@@ -1,9 +1,19 @@
 const calculator = require('./calculator');
 
+// addition
 test('adds two numbers', () => {
   expect(calculator.add(3, 9)).toBe(12);
 });
 
 test('returns error if an argument is not a number', () => {
   expect(calculator.add('3', 6)).toMatch(/Please enter two numbers/)
-})
+});
+
+// subtraction
+test('subtract one positive number from another', () => {
+  expect(calculator.subtract(9, 3)).toBe(6);
+});
+
+test('returns error if an argument is not a number', () => {
+  expect(calculator.subtract('6', 3)).toMatch(/Please enter two numbers/)
+});
