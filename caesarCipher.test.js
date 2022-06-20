a566edc2097e1ca6f288413a13bb7a024d8bf3e2
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-06-20 12:14:55 +0200

    Add test to caesarCipher.test: Case Sensitive Test
    Block first 2 tests for now

--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -1,9 +1,15 @@
 const caesarCipher = require('./caesarCipher');
 
-test('tests wrapping from z to a', () => {
+xtest('tests wrapping from z to a', () => {
   expect(caesarCipher("xyz", 1)).toMatch(/yza/)
 });
 
-test('test with multiple words', () => {
-  expect(caesarCipher('this works just fine', 1)).toMatch(/uijt xpslt kvtu gjof/)
+xtest('test with multiple words', () => {
+  expect(caesarCipher('this works just fine', 1))
+    .toMatch(/uijt xpslt kvtu gjof/)
+});
+
+test('Case Sensitive Test', () => {
+  expect(caesarCipher('Title Case Test', 5))
+    .toMatch(/Ynyqj Hfxj Yjxy/)
 });
