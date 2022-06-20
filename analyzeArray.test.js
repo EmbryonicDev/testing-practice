a156d2abd1c765baff85ec44955642a43f9a90cf
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-06-20 20:03:01 +0200

    Add test: returns error if any array is empty

--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -14,3 +14,8 @@ test('returns error if any array value != Number', () => {
   expect(analyzeArray([14, 8, '15', 23, 47]))
     .toMatch(/Please only enter an array with NUMBERS/)
 });
+
+test('returns error if any array is empty', () => {
+  expect(analyzeArray([]))
+    .toMatch(/Please do not enter an empty array/)
+});
